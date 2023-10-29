ba10a939e61770c5cf888e9b011f262d89fb84fc
Wilson McDade <wilson.mcdade@gmail.com>
2023-10-29 12:06:23 -0400

    made config built of env vars

---
+++ config.env.py
@@ -0,0 +1,14 @@
+import os
+
+DBNAME = os.getenv("DBNAME", default="tunnelvision")
+DBUSER = os.getenv("DBUSER", default="")
+DBPWD =  os.getenv("DBPWD", default="")
+DBHOST = os.getenv("DBHOST", default="postgres.csh.rit.edu")
+DBPORT = os.getenv("DBPORT", default="5432")
+
+S3_URL = os.getenv("S3_URL", default="s3.csh.rit.edu")
+S3_KEY = os.getenv("S3_URL", default="")
+S3_SECRET = os.getenv("S3_SECRET", default="")
+BUCKET_NAME = os.getenv("BUCKET_NAME", default="tunnelvision")
+
+DEBUG = os.getenv("DEBUG", default=False)
