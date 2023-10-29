bf73ea7515ab01db910818cdf49565d991a414dd
Wilson McDade <wilson.mcdade@gmail.com>
2023-10-29 18:57:26 -0400

    Update

--- config.env.py
+++ config.env.py
@@ -7,7 +7,7 @@ DBHOST = os.getenv("DBHOST", default="postgres.csh.rit.edu")
 DBPORT = os.getenv("DBPORT", default="5432")
 
 S3_URL = os.getenv("S3_URL", default="s3.csh.rit.edu")
-S3_KEY = os.getenv("S3_URL", default="")
+S3_KEY = os.getenv("S3_KEY", default="")
 S3_SECRET = os.getenv("S3_SECRET", default="")
 BUCKET_NAME = os.getenv("BUCKET_NAME", default="tunnelvision")
 
