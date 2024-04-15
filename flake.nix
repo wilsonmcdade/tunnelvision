f992fba51ebbc4c2f346b7b0a8b3350b98d10d61
Sootpaws <sootpaws@proton.me>
2024-04-15 09:19:56 -0400

    Create flake.nix with development shell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+    description = "Development shell for Tunnelvision";
+
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { nixpkgs, flake-utils, ... }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = import nixpkgs { inherit system; };
+            in { devShells.default = pkgs.mkShell {
+                packages = with pkgs; with python311Packages; [
+                    python3
+                    boto
+                    flask
+                    pillow
+                    psycopg2
+                    werkzeug
+                ];
+            }; }
+        );
+}
