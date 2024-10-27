b61ddf7d64db9be5aa8cc20fdc2fffa54b699f13
Adrian Edwards <adrian@adriancedwards.com>
2024-10-26 21:36:12 -0400

    start converting s3 file to a class structure and migrating to boto3

--- s3.py
+++ s3.py
@@ -5,55 +5,71 @@
 import mimetypes
 import boto
 import boto.s3.connection
+import boto3
 
-def get_file(bucket_name, file_hash, new_file_name, key, secret):
-    with open(new_file_name, "wb") as f:
-        boto.utils.fetch_file("s3://{0}/{1}".format(bucket_name,file_hash), file=f, username=key, password=secret)
+class S3Bucket:
 
-def get_file_s3(bucket, file_hash):
-    key = bucket.get_key(file_hash)
-    # Generates presigned URL that lasts for 60 seconds (1 minute)
-    # If streaming begins prior to the time cutoff, s3 will allow
-    # for the streaming to continue, uninterrupted.
-    if (key == None):
-        print("Failed to fetch {0}".format(file_hash))
-        url = "../static/images/csh_tilted.png"
-    else:
-        url = key.generate_url(90, query_auth=True)
-    return url
+    def __init__(self, name, key, secret, endpoint):
+        self.name = name
 
-def get_file_list(bucket):
-    # List all files in the bucket
-    return bucket.list()
+        self._session = boto3.session.Session()
 
-def get_date_modified(bucket, file_hash):
-    # Get date modified for a specific file in the bucket
-    date =  bucket.get_key(file_hash).last_modified
-    return date[:(date.index(":") - 2)]
+        self._client = self._session.client(
+            service_name='s3',
+            aws_access_key_id=key,
+            aws_secret_access_key=secret,
+            endpoint_url=endpoint,
+        )
 
-def upload_file(bucket, file_hash, f, filename=""):
-    # Create bucket key with filename
-    key = bucket.new_key(file_hash)
-    # Set content type
-    # There is most certainly a better way to do this but w/e
-    if filename == "":
-        content_type = mimetypes.guess_type(f.filename)[0]
-    else:
-        content_type = mimetypes.guess_type(filename)[0]
-    # Upload the file
-    key.set_contents_from_file(f, headers={"Content-Type": content_type})
+    # unused
+    # def get_file(self, bucket_name, file_hash, new_file_name, key, secret):
+    #     with open(new_file_name, "wb") as f:
+    #         boto.utils.fetch_file(f"s3://{bucket_name}/{file_hash}", file=f, username=key, password=secret)
 
-def remove_file(bucket, file_hash):
-    # Does anybody read these comments
-    bucket.delete_key(file_hash)
+    def get_file_s3(self, file_hash):
+        key = self._client.get_objects_v2(self.name, file_hash)
+        # Generates presigned URL that lasts for 60 seconds (1 minute)
+        # If streaming begins prior to the time cutoff, s3 will allow
+        # for the streaming to continue, uninterrupted.
+        if (key == None):
+            print("Failed to fetch {0}".format(file_hash))
+            url = "../static/images/csh_tilted.png"
+        else:
+            url = key.generate_url(90, query_auth=True)
+        return url
 
-def get_bucket(s3_url, s3_key, s3_secret, bucket_name):
-    # Establish s3 connection through boto
-    # Formatting long paramter strings is never fun
-    conn = boto.connect_s3(
-                aws_access_key_id = s3_key,
-                aws_secret_access_key = s3_secret,
-                host = s3_url,
-                calling_format = boto.s3.connection.OrdinaryCallingFormat())
-    # Return the bucket rather than the entire resource
-    return conn.lookup(bucket_name)
+    def get_file_list(self):
+        # List all files in the bucket
+        return self._client.list_objects_v2(self.name)
+
+    def get_date_modified(self, file_hash):
+        # Get date modified for a specific file in the bucket
+        date =  self._client.get_object(self.name, file_hash).get("LastModified")
+        # TODO: this may not work with datetime objects
+        return date[:(date.index(":") - 2)]
+
+    def upload_file(self, file_hash, f, filename=""):
+
+        file_path = filename if filename != "" else f.filename
+
+        content_type = mimetypes.guess_type(file_path)[0]
+
+        self._client.upload_file(file_path, self.name, file_hash, {"ContentType": content_type})
+
+    def remove_file(self, file_hash):
+        # Does anybody read these comments
+        # yes
+        self._client.delete_object(self.name, file_hash)
+
+    def get_bucket(self):
+        # Establish s3 connection through boto
+        # Formatting long paramter strings is never fun
+        return self._client.get_bucket(self.name)
+        # s3 = client.resource('s3')
+        # conn = boto.connect_s3(
+        #             aws_access_key_id = s3_key,
+        #             aws_secret_access_key = s3_secret,
+        #             host = s3_url,
+        #             calling_format = boto.s3.connection.OrdinaryCallingFormat())
+        # Return the bucket rather than the entire resource
+        # return s3.Bucket(bucket_name)
